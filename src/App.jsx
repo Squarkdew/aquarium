5a070a9988224e981a2c12943836a3c85b15ff85
Мухаммад Якубов <main@MacBook-Air-Muhammad.local>
2024-06-06 18:39:50 +0600

    header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+
+function App() {
+  const getFish = async () => {
+    
+  }
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
